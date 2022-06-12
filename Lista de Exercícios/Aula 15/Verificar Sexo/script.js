999f9bbfdcd1d026e5003b65cef48bd0748b99a8
valerio-figueira <j.valerio.figueira@gmail.com>
2022-06-12 10:16:16 -0300

    new files

--- Lista de Exercícios/Aula 15/Verificar Sexo/script.js
+++ Lista de Exercícios/Aula 15/Verificar Sexo/script.js
@@ -1,23 +1,28 @@
 let g = ['F', 'M'] 
 
+let p = document.createElement('p')
+let img = document.createElement('img')
+
 function verificar(){
     let text = document.getElementById('gender')
     let res = document.getElementById('res')
     
     let gender = String(text.value)
-
-    let p = document.createElement('p')
+    
     p.style.color = 'black'
     
     res.appendChild(p)
+    res.appendChild(img)
 
     if(gender == 'F' || gender == 'f'){
-        p.innerText = 'É feminino'
+        p.innerText = 'É mulher!'
+        img.setAttribute('src', 'imgs/mulher.png')
     } else if(gender == 'M' || gender == 'm'){
-        p.innerText = 'É masculino'
+        p.innerText = 'É homem!'
+        img.setAttribute('src', 'imgs/homem.png')
     } else{
         p.style.color = 'crimson'
-        p.innerText = `[ERRO] Insira um valor válido`
+        p.innerText = `[ERRO] Não é humano!`
+        img.setAttribute('src', '')
     }
-
 }
